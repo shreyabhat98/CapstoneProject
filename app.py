122e11f0ee506eaae7bfdda16d59b408a93c0030
Sshreya Y <yshreybhat@gmail.com>
2023-04-24 09:02:30 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1 style='text-align: center;'>Hello World,My name is Sshreya!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
